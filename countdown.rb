27541a2c6f9b9112d45d4605e3f21c03a83e6715
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-17 13:30:26 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown(num)
   while num >= 0 
-    puts "#{num} SECOND (S) !\n"
+    puts "#{num} SECOND(S)!\n"
      num -= 1
   end
   return "HAPPY NEW YEAR!"
@@ -10,7 +10,7 @@ end
 
 def countdown_with_sleep(num)
     while num >= 0 
-    puts "#{num} SECOND (S) !\n"
+    puts "#{num} SECOND(S)!\n"
      num -= 1
      sleep(1)
   end
